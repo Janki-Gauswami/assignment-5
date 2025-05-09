fedb65efa5ea34dbc3a6ac2e613a13b6b5dbe08c
Janki-Gauswami <156094604+Janki-Gauswami@users.noreply.github.com>
2025-05-09 10:26:11 +0530

    Update assignment 5-b.py

--- assignment 5-b.py
+++ assignment 5-b.py
@@ -1,16 +1,9 @@
 list = [1,2,3,4,5,6,7,8,9,10]
 
-print("original list",list)
-
-n = len(list)//2
-
-newlist = []
-
-for i in range(n):
-    newlist.append(list[i])
+newlist = list[:5]
 
 print("Extracted first five element is: ",newlist)
 
-newlist.reverse()
+reversed_newlist = newlist[::-1]
 
-print("Reversed extracted elements: ",newlist)
+print("Reversed extracted elements: ",reversed_newlist)
